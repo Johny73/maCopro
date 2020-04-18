affcb4fba2b39dde7522e643cd2b2d776ccccf14
Johny HEIJLEN <j_heijlen@hotmail.com>
2020-04-18 11:08:06 +0200

    ajout form nvx évent journal + enregistrement dans BDD

--- src/Controller/ComptaController.php
+++ src/Controller/ComptaController.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 /**
  * @Route("/compta")
@@ -32,14 +33,14 @@ class ComptaController extends AbstractController
     }
 
         /**
-     * @Route("/new", methods={"GET", "POST"})
+     * @Route("/new", methods={"POST", "GET"})
      */
     public function new(Request $request, EntityManagerInterface $manager)
     {  
         $defaultData = ['message' => 'Sélectionnez un compte'];
         $form = $this->createFormBuilder($defaultData)
         ->add('date', DateType::class, ['widget' => 'single_text',] )
-        ->add('Montant', MoneyType::class)
+        ->add('montant', NumberType::class)
         ->add('choixCompte1', EntityType::class, [
             'class' => Comptes::class,
             'choice_label' => 'labelCompte',
@@ -50,18 +51,24 @@ class ComptaController extends AbstractController
             'choice_label' => 'labelCompte',
             'label' => 'Compte à créditer',
         ])
-        ->add('Commentaire', TextType::class)
-        ->add('saveg', SubmitType::class, ['label' => 'Valider'])
+        ->add('commentaire', TextType::class)
+        ->add('save', SubmitType::class, ['label' => 'Valider','attr' => ['class' => 'btn btn-primary'],])
 
         ->getForm();
 
-        if ($form->isSubmitted() /*&& $form->isValid()*/) {
-            $this->ajoutJournal($_POST['montant']);
-             /*   return $this->redirectToRoute('app_compta_index');*/
+        $form->handleRequest($request);
+      
+        if ($form->isSubmitted() && $form->isValid() ) {
+            $date = $form['date']->getData();
+            $montant = $form['montant']->getData();
+           
+            
+            $compteDebit = $form['choixCompte1']->getData()->getId();
+            $compteCredit = $form['choixCompte2']->getData()->getId();
+            $commentaire = $form['commentaire']->getData();
+            $this->ajoutJournal($date, $montant, $compteDebit, $compteCredit, $commentaire);
+            return $this->redirectToRoute('app_compta_index');
             }
-           
-         
-           
        
         return $this->render('compta/new.html.twig', [
             'controller_name' => 'ComptaController',
@@ -70,13 +77,16 @@ class ComptaController extends AbstractController
         ]);
     }
 
-    public function ajoutJournal($montant): Response
+    public function ajoutJournal($date, $montant, $compteDebit, $compteCredit, $commentaire): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
 
         $journalEvent = new Journal();
+        $journalEvent->setDate($date);
         $journalEvent->setMontant($montant);
-       
+        $journalEvent->setCompteDebit($compteDebit);
+        $journalEvent->setCompteCredit($compteCredit);
+        $journalEvent->setCommentaire($commentaire);
 
         // tell Doctrine you want to (eventually) save the Product (no queries yet)
         $entityManager->persist($journalEvent);
@@ -84,7 +94,7 @@ class ComptaController extends AbstractController
         // actually executes the queries (i.e. the INSERT query)
         $entityManager->flush();
 
-        return new Response('L\écriture a été ajouté ');
+        return new Response('écriture a été ajouté ');
     }
 
 
