2f2a877fef437a1230b60d43b791a4de8e16b155
Johny HEIJLEN <j_heijlen@hotmail.com>
2020-05-12 16:58:38 +0200

    ajout mailController pour envoi des mails

---
+++ src/Controller/MailController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MailController extends AbstractController
+{
+    /**
+     * @Route("/email")
+     */
+    public function sendEmail(MailerInterface $mailer)
+    {
+        $email = (new Email())
+            ->from('johny.heijlen@gmail.com')
+            ->to('j_heijlen@hotmail.com')
+            ->addTo('test@dawan.fr')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            //->attachFromPath('/path/to/documents/terms-of-use.pdf')
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+            $mailer->send($email);
+         
+
+         
+         return;
+
+        
+    }
+}
